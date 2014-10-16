1e4267b304b4647bd59c06ab6901f62b70c85b32
NIL <NIL@NIL-HP>
2014-10-16 23:55:25 +0200

    Refactored square into shape

--- ShapeLib/src/org/nla/shapelib/Square.java
+++ ShapeLib/src/org/nla/shapelib/Square.java
@@ -1,20 +1,8 @@
 package org.nla.shapelib;
 
-public class Square implements Shape {
-
-	private double width;
+public class Square extends Rectangle {
 
 	public Square(double width) {
-		this.width = width;
-	}
-
-	@Override
-	public double area() {
-		return this.width * this.width;
-	}
-
-	@Override
-	public double perimeter() {
-		return 4 * this.width;
+		super(width, width);
 	}
 }
